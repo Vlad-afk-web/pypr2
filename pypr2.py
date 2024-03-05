820c14640422554b5d4d569a930bf81c2dc628da
Vlad-afk-web <74813221+Vlad-afk-web@users.noreply.github.com>
2024-03-05 12:31:48 +0200

    Add files via upload

---
+++ pypr2.py
@@ -0,0 +1,115 @@
+a = 3
+b = 6
+
+# #1
+
+# print('Завдання 1 \n ' , " = " , a , b)
+
+# #2
+
+# print("Завдання 2 \n ", b > a and a < b, b == b and a == a, b < a and a > b, b != b and a != a)
+
+# 3
+
+# print("Завдання 3 \n ", b < a or not a > b, b == a or a == a, b <= a or a > b, a < 1 or a > 9)
+
+#4
+
+# var_str = 'hewwo wowd'
+# var_str2 = 'hello world'
+
+# print("Завдання 4 \n ", var_str == var_str2 or var_str2 != var_str)
+
+#5
+
+c = -3
+
+# if a > 0:
+#     print("Завдання 5 \n Число більше нуля")
+
+# if c < 0:
+#     print(" Число менше нуля")
+
+#  #6
+
+# if a > 0:
+#     print("Завдання 6 \n", 1)
+# else:
+#     print("Завдання 6 \n", -1)
+
+#7
+   
+# print("Введіть значення для завдання №7 ")
+
+# l = int(input("Введіть першу змінну "))
+# e = int(input("Введіть другу змінну "))
+
+# if l > e:
+#     f = l - e
+# elif l < e:
+#     f = l + e
+# else:
+#     f = l
+
+# print("Завдання 7 \n ", "Значення третьої змінної = " , f)
+
+ # 8
+
+# if a > b and a > c:
+#     d = a
+# elif b > a and b > c:  
+#     d = b
+# else:
+#     d = c
+
+# if a < b and a < c:
+#     e = a
+# elif b < a and b < c:  
+#     e = b
+# else:
+#     e = c
+
+# print("Завдання 8 \n " , d , e)
+
+#9
+
+# fib1 = 0
+# fib2 = 1
+# n = 20
+# i = 3
+# while i <= n:
+#     fib_sum = fib1 + fib2
+#     fib1 = fib2
+#     fib2 = fib_sum
+#     if i >= 5:
+#         print (fib_sum)
+#     i = i + 1
+
+
+# p = 1
+# while p <= 20:
+#     if p%2 == 0:
+#         print(p)
+#     p += 1
+
+
+# o = -1
+# while o >= -21:
+#     if o%3 == 0:
+#         print(o)
+#     o -= 1
+
+#10
+
+# sum = 0
+# for i in range(6):
+#     chislo = int(input("Введіть ціле число: "))
+#     if chislo % 5 == 0:
+#         sum += chislo
+# print("Сума чисел = " ,sum)
+
+
+
+
+
+
